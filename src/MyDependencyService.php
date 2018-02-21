b85a30fa9f2b24ecc7135b970936535fdfe3220f
morinluc0 <lmorin@coldfrontlabs.ca>
2018-02-21 12:37:35 -0500

    Update MyDependencyService.php

--- src/MyDependencyService.php
+++ src/MyDependencyService.php
@@ -1,1 +1,23 @@
+<?php
 
+namespace Drupal\nrc_example;
+
+use Drupal\Core\Entity\EntityManageInterface;
+
+/**
+ * Service for NRC example.
+ */
+
+class MyDependencyService {
+  
+  protected entity_manage;
+  
+  public function __construct(EntityManageInterface $entity) {
+    $this->entity_manage = $entity;
+  }
+  
+  public function getEntityUserMail($id) {
+    $user = $this->entity_manage->getStorage(‘user’)->load($id);
+    $user->getEmail();
+  }
+}
