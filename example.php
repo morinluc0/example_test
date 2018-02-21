b14c80fc9b62cf76a8475d8f3a747b1d3f092df4
Luc Morin <lmorin@coldfrontlabs.ca>
2018-02-21 11:52:37 -0500

    new file.

---
+++ example.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Drupal\nrc_example\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class NRCExampleForm extends FormBase {
+ public function getFormId() {
+    return 'my_example_module_form';
+ }
+ public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['my_example'] = [
+      '#markup' => t('Hello NRC module'),
+    ];
+    return $form;
+ }
+}
+
